823080137f668259ff18f16fb1cfb8f4334fa8da
Christiane Pousa Ribeiro <pousa.christiane@gmail.com>
2015-06-23 15:19:18 +0200

    unify resources and user plugin for LSF jobs

--- src/jobstatus.c
+++ src/jobstatus.c
@@ -30,7 +30,8 @@
 
 typedef struct lsf_conf_file
 {
-  char *serverdir;	
+  char serverdir[128];	
+  char envdir[128];
 
 } lsf_conf_file_t;
 
@@ -38,14 +39,17 @@ static lsf_conf_file_t *lsf_conf = NULL;
 
 static const char *config_keys[] = 
 {
+	"LSF_SERVERDIR",
 	"LSF_ENVDIR",
 };
 
 static int config_keys_num = STATIC_ARRAY_SIZE (config_keys);
 
+#define JOB_NAME_LEN 256 
+
 typedef struct  jobstatus
 {
-	char name[256];
+	char name[JOB_NAME_LEN];
 	unsigned long nrun;
 	unsigned long npend;
 	unsigned long ncores_run;
@@ -54,9 +58,20 @@ typedef struct  jobstatus
 	struct jobstatus *next;
 } jobstatus_t;
 
-static jobstatus_t *list_head_g = NULL;
+typedef struct  jobresources
+{
+    char jobId[JOB_NAME_LEN];
+        unsigned long ncores;
+        unsigned long runtime;
+    unsigned long memory;
+    unsigned long scratch;
+        struct jobresources *next;
+} jobresources_t;
 
-static void jobstatus_submit (jobstatus_t *js)
+static jobstatus_t 	*list_head_g = NULL;
+static jobresources_t 	*list_head_res_g = NULL;
+
+static void jobstatus_submit_user (jobstatus_t *js)
 {
     value_t values[2];
     value_list_t vl = VALUE_LIST_INIT;
@@ -67,22 +82,22 @@ static void jobstatus_submit (jobstatus_t *js)
     sstrncpy (vl.plugin, "user", sizeof (vl.plugin));
     sstrncpy (vl.plugin_instance, js->name, sizeof (vl.plugin_instance));
 
-	sstrncpy (vl.type, "js_nrun", sizeof (vl.type));
+    sstrncpy (vl.type, "js_nrun", sizeof (vl.type));
     vl.values[0].gauge = js->nrun;
     vl.values_len = 1;
     plugin_dispatch_values (&vl);
 
-	sstrncpy (vl.type, "js_npend", sizeof (vl.type));
+    sstrncpy (vl.type, "js_npend", sizeof (vl.type));
     vl.values[0].gauge = js->npend;
     vl.values_len = 1;
     plugin_dispatch_values (&vl);
 
-	sstrncpy (vl.type, "js_ncores_run", sizeof (vl.type));
+    sstrncpy (vl.type, "js_ncores_run", sizeof (vl.type));
     vl.values[0].gauge = js->ncores_run;
     vl.values_len = 1;
     plugin_dispatch_values (&vl);
 
-	sstrncpy (vl.type, "js_ncores_pend", sizeof (vl.type));
+    sstrncpy (vl.type, "js_ncores_pend", sizeof (vl.type));
     vl.values[0].gauge = js->ncores_pend;
     vl.values_len = 1;
     plugin_dispatch_values (&vl);
@@ -93,7 +108,40 @@ static void jobstatus_submit (jobstatus_t *js)
     plugin_dispatch_values (&vl);
 }
 
-static void jobstatus_list_add (jobstatus_t *js)
+static void jobstatus_submit_resources (jobresources_t *js)
+{
+    value_t values[2];
+    value_list_t vl = VALUE_LIST_INIT;
+
+    vl.values = values;
+    vl.values_len = 2;
+    sstrncpy (vl.host, hostname_g, sizeof (vl.host));
+    sstrncpy (vl.plugin, "job", sizeof (vl.plugin));
+    sstrncpy (vl.plugin_instance, js->jobId, sizeof (vl.plugin_instance));
+
+        sstrncpy (vl.type, "js_ncores", sizeof (vl.type));
+    vl.values[0].gauge = js->ncores;
+    vl.values_len = 1;
+    plugin_dispatch_values (&vl);
+
+        sstrncpy (vl.type, "js_runtime", sizeof (vl.type));
+    vl.values[0].gauge = js->runtime;
+    vl.values_len = 1;
+    plugin_dispatch_values (&vl);
+
+        sstrncpy (vl.type, "js_memory", sizeof (vl.type));
+    vl.values[0].gauge = js->memory;
+    vl.values_len = 1;
+    plugin_dispatch_values (&vl);
+
+        sstrncpy (vl.type, "js_scratch", sizeof (vl.type));
+    vl.values[0].gauge = js->scratch;
+    vl.values_len = 1;
+    plugin_dispatch_values (&vl);
+
+}
+
+static void jobstatus_list_add_user (jobstatus_t *js)
 {
 	jobstatus_t *new;
 
@@ -137,18 +185,144 @@ static void jobstatus_list_add (jobstatus_t *js)
 	}	
 }
 
+static void jobresources_list_add_res (jobresources_t *js)
+{
+        jobresources_t *new;
+
+        new = (jobresources_t *) malloc (sizeof(jobresources_t));
+    if (new == NULL)
+         return;
+
+        memset (new, 0, sizeof (jobresources_t));
+
+    sstrncpy(new->jobId, js->jobId, sizeof(new->jobId));
+    new->ncores = js->ncores;
+    new->runtime = js->runtime;
+    new->memory = js->memory;
+    new->scratch = js->scratch;
+        new->next = NULL;
+
+        if (list_head_res_g == NULL){
+                list_head_res_g = new;
+        }
+        else{
+
+                jobresources_t *ps = list_head_res_g;
+                jobresources_t *last_ps;
+                while(ps != NULL && strcmp(ps->jobId,js->jobId) != 0)
+                {
+                        last_ps = ps;
+                        ps = ps->next;
+                }
+
+                if (ps == NULL){
+                        last_ps->next = new;
+
+                }
+                else{
+            ps->ncores = new->ncores;
+            ps->memory = new->memory;
+            ps->scratch = new->scratch;
+            ps->runtime = new->runtime;
+                }
+        }
+
+}
+
+
 static void jobstatus_list_reset (void)
 {
     jobstatus_t *ps = list_head_g;
+    jobresources_t *ps_res = list_head_res_g;
+
+    while( list_head_res_g!= NULL) {
+            ps_res = list_head_res_g;
+            list_head_res_g = list_head_res_g->next;
+            free(ps_res);
+        }	
     
     while( list_head_g!= NULL) {
             ps = list_head_g;
             list_head_g = list_head_g->next;
             free(ps);
         }
+
+	if (lsf_conf != NULL)
+		free(lsf_conf);	
 }
 
-static jobstatus_t* read_single_job (struct jobInfoEnt *job, jobstatus_t *js)
+/*
+ * ** Parses LSF's RR string and extracts mem=XXXX
+ * ** WARNING: THIS RETURNS MB - MOST OTHER VALUES ARE IN KB
+ * */
+static int get_rr_mem(char *rr) {
+        char match[] = "mem=";      /* prefix to search                         */
+        char *scr = NULL;           /* scratch buffer-pointer                   */
+        int rmem = -1;              /* return value -> requested memory         */
+        int xoff = 0;               /* offset of first integer value after mem= */
+        int mlen = strlen(match);
+        int rlen = strlen(rr);
+        int i;
+
+
+        for(i=mlen;i<rlen;i++) {
+                if(xoff == 0 && memcmp(&rr[i-mlen], match, mlen) == 0) {
+                        xoff = i;
+                }
+                else if(xoff && (rr[i] < '0' || rr[i] > '9')) {
+                        break;
+                }
+        }
+        if(xoff) { /* hit is between i <-> xoff */
+                if( (scr = malloc( 1+i-xoff )) != NULL &&
+                       (snprintf(scr, 1+i-xoff, "%s", &rr[xoff])) > 0 ) {
+                        rmem = atoi(scr);
+                }
+        }
+
+        if(scr != NULL)
+                free(scr);
+
+        return rmem;
+}
+
+/*
+ *  * ** Parses LSF's RR string and extracts scratch=XXXX
+ *   * ** WARNING: THIS RETURNS MB - MOST OTHER VALUES ARE IN KB
+ *    * */
+static int get_rr_scratch(char *rr) {
+        char match[] = "scratch=";      /* prefix to search                         */
+        char *scr = NULL;           /* scratch buffer-pointer                   */
+        int rscr = 0;              /* return value -> requested scratch         */
+        int xoff = 0;               /* offset of first integer value after scratch= */
+        int slen = strlen(match);
+        int rlen = strlen(rr);
+        int i;
+
+        for(i=slen;i<rlen;i++) {
+                if(xoff == 0 && memcmp(&rr[i-slen], match, slen) == 0) {
+                        xoff = i;
+                }
+                else if(xoff && (rr[i] < '0' || rr[i] > '9')) {
+                        break;
+                }
+        }
+
+        if(xoff) { /* hit is between i <-> xoff */
+                if( (scr = malloc( 1+i-xoff )) != NULL &&
+                       (snprintf(scr, 1+i-xoff, "%s", &rr[xoff])) > 0 ) {
+                        rscr = atoi(scr);
+                }
+        }
+
+        if(scr != NULL)
+                free(scr);
+
+        return rscr;
+}
+
+
+static jobstatus_t* read_single_job_user (struct jobInfoEnt *job, jobstatus_t *js)
 {
     if (job == NULL)
         return NULL;
@@ -174,49 +348,72 @@ static jobstatus_t* read_single_job (struct jobInfoEnt *job, jobstatus_t *js)
     return js;
 }
 
+static jobresources_t* read_single_job_res (struct jobInfoEnt *job, jobresources_t *js)
+{
+    if (job == NULL)
+        return NULL;
+
+    char jobId[JOB_NAME_LEN];
+
+    ssnprintf (jobId, sizeof (jobId), "%lli", job->jobId);
+    sstrncpy(js->jobId, jobId, sizeof(js->jobId));
+
+    if (job->status == 4){
+                js->ncores = job->submit.numProcessors;
+        js->runtime = job->jRusageUpdateTime - job->startTime;
+        js->memory = get_rr_mem(job->submit.resReq);
+        js->scratch = get_rr_scratch(job->submit.resReq);
+    }
+	else
+		js = NULL;	   	
+
+        js->next = NULL;
+
+
+    return js;
+}
+
+
 static int add_lsf_conf(const char *key, const char *value)
 {
 	lsf_conf = (lsf_conf_file_t *) malloc (sizeof(lsf_conf_file_t));
-    memset (lsf_conf, '\0', sizeof (lsf_conf_file_t));
+	
+	 memset (lsf_conf, 0, sizeof (lsf_conf_file_t));
 
  	if ((strcasecmp (key, "LSF_SERVERDIR") == 0))
     {
-	    lsf_conf->serverdir = strdup(value);
+	    sstrncpy(lsf_conf->serverdir,value, sizeof(lsf_conf->serverdir));
+
+	
+
             if ( lsf_conf->serverdir == NULL)
                         return (-1);
 	}	
+	else if ((strcasecmp (key, "LSF_ENVDIR") == 0))
+    {
+            sstrncpy(lsf_conf->envdir, value, sizeof(lsf_conf->envdir));
+            if ( lsf_conf->envdir == NULL)
+                        return (-1);
+        }
+
+	putenv(lsf_conf->envdir);
+	putenv(lsf_conf->serverdir);
 	return 0;
 }
 
 static int jobstatus_config (const char *key, const char *value)
 {
-	char *new_val;
-	char *fields[2];
-  	int fields_num;
-
-	new_val = strdup (value);
-  	if (new_val == NULL)
-    		return (-1);
-
-	fields_num = strsplit (new_val, fields, STATIC_ARRAY_SIZE (fields));
-
-  	if ((fields_num < 1) || (fields_num > 2))
-  	{
-    		sfree (new_val);
-    		return (-1);
-  	}
-
-	if ((strcasecmp (key, "LSF_ENVDIR") == 0))
+	
+	if ((strcasecmp (key, "LSF_SERVERDIR") == 0))
 	{
-		if (fields_num != 1)
-                {
-                   ERROR ("jobstatus plugin: Invalid number of fields for option "
-                        "`%s'. Got %i, expected 1.", key, fields_num);
-                   return (-1);
-                }
-		else
-		   add_lsf_conf(key, fields[0]);
+		add_lsf_conf(key, value);
 	}
+	else if (strcasecmp (key, "LSF_ENVDIR") == 0)
+	{
+		add_lsf_conf(key, value);
+	}
+	else
+		return (-1);
 
 	return (0);
 }
@@ -224,8 +421,6 @@ static int jobstatus_config (const char *key, const char *value)
 static int init(void)
 {
 	//If appName is NULL, the logfile $LSF_LOGDIR/bcmd receives LSBLIB transaction
-	putenv(lsf_conf->serverdir);
-	
 	if (lsb_init(NULL) < 0)
 	{
 		ERROR("Jobstatus plugin: Initialization problems");
@@ -237,7 +432,8 @@ static int init(void)
 
 static int jobstatus_read (void)
 {
-	jobstatus_t *js;	
+	jobstatus_t *js_user;	
+	jobresources_t *js_res;
 
 	//jobs state
 	int jopts = 0;
@@ -278,35 +474,52 @@ static int jobstatus_read (void)
 			return (-1);
 		}
 
-		js = (jobstatus_t *) malloc (sizeof(jobstatus_t));
-		if (js == NULL)
+		js_user = (jobstatus_t *) malloc (sizeof(jobstatus_t));
+		if (js_user == NULL)
 			return (-1);		
 
-		if ( read_single_job(job, js) == NULL )
+		if ( read_single_job_user(job, js_user) == NULL )
 		{
-			memset(js->name,' ',sizeof(js->name));
-			js->nrun = -1;
-			js->npend = -1;
-			js->ncores_run = -1;
-			js->ncores_pend = -1;
-            js->ndep = -1;
+			memset(js_user->name,' ',sizeof(js_user->name));
+			js_user->nrun = -1;
+			js_user->npend = -1;
+			js_user->ncores_run = -1;
+			js_user->ncores_pend = -1;
+            		js_user->ndep = -1;
 		}
         
-        if (job->status == 4){
-            struct jobDepRequest jobdepReq;
-            jobdepReq.jobId = job->jobId;
-            jobdepReq.options = QUERY_DEPEND_UNSATISFIED;
-            struct jobDependInfo *jobDep = lsb_getjobdepinfo(&jobdepReq); 
-            js->ndep = jobDep->numJobs;
-        }
-        else
-            js->ndep = 0;
-
-		jobstatus_list_add(js);
+        	if (job->status == 4){
+            		struct jobDepRequest jobdepReq;
+            		jobdepReq.jobId = job->jobId;
+            		jobdepReq.options = QUERY_DEPEND_UNSATISFIED;
+            		struct jobDependInfo *jobDep = lsb_getjobdepinfo(&jobdepReq); 
+            		js_user->ndep = jobDep->numJobs;
+        	}
+        	else
+            		js_user->ndep = 0;
+		
+		js_res = (jobresources_t *) malloc (sizeof(jobresources_t));
+                if (js_res == NULL)
+                        return (-1);
+	
+		if ( read_single_job_res(job, js_res) == NULL )
+                {
+                        memset(js_res->jobId,' ',sizeof(js_res->jobId));
+                        js_res->ncores = 0;
+                	js_res->runtime = 0;
+                	js_res->memory = 0;
+                        js_res->scratch = 0;
+                }
+		
+		jobresources_list_add_res(js_res);
+		jobstatus_list_add_user(js_user);
 	}		
 
-	for (js=list_head_g; js != NULL; js=js->next)
-		jobstatus_submit(js);
+	for (js_user=list_head_g; js_user != NULL; js_user=js_user->next)
+		jobstatus_submit_user(js_user);
+
+	for (js_res=list_head_res_g; js_res != NULL; js_res=js_res->next)
+                jobstatus_submit_resources(js_res);
 
 	return (0);
 }
